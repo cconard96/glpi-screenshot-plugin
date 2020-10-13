ee01f59e18572c936a1537c65f1c991cd5ca7ecf
cconard96 <cconard96@gmail.com>
2020-10-12 20:54:59 -0400

    Implement back-end for screen recordings (video)

--- ajax/screenshot.php
+++ ajax/screenshot.php
@@ -35,43 +35,92 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
    // Bad request method
    die(405);
 }
-if (!isset($_POST['img'], $_POST['itemtype'], $_POST['items_id'])) {
-   // Missing required data
-   die(400);
+if (!isset($_POST['itemtype'], $_POST['items_id'])) {
+   if (!isset($_POST['img']) || !isset($_FILES['blob'])) {
+      // Missing required data
+      die(400);
+   }
 }
 
 $format_ext_map = [
    'image/png'    => 'png',
-   'image/jpeg'   => 'jpg'
+   'image/jpeg'   => 'jpg',
+   'video/webm'   => 'webm'
 ];
 
 if (!isset($_POST['format'], $format_ext_map[$_POST['format']])) {
    // Unsupported format or missing format
    die(400);
 }
-
 $ext = $format_ext_map[$_POST['format']];
-// Name format: Screenshot + Timestamp + random 5 character hex + extension
-$file_name = 'Screenshot ' . $_SESSION['glpi_currenttime'] . '-' . sprintf('%05X', random_int(0, 1048575)) . '.' . $ext;
-$data = file_get_contents($_POST['img']);
-// Save image to tmp then add document and link. Adding the document will cleanup the temp file for us.
-$file = fopen(GLPI_TMP_DIR . '/' . $file_name, 'wb');
-fwrite($file, $data);
-fclose($file);
 
-$doc = new Document();
-$doc_id = $doc->add([
-   'name'         => $file_name,
-   '_filename'    => [$file_name]
-]);
-$doc_item = new Document_Item();
-$doc_item->add([
-   'documents_id' => $doc_id,
-   'itemtype'     => $_POST['itemtype'],
-   'items_id'     => $_POST['items_id'],
-]);
+if (isset($_POST['img'])) {
+   // Handle screenshot upload
+
+   // Name format: Screenshot + Timestamp + random 5 character hex + extension
+   $file_name = 'Screenshot ' . $_SESSION['glpi_currenttime'] . '-' . sprintf('%05X', random_int(0, 1048575)) . '.' . $ext;
+   if (!Document::isValidDoc($file_name)) {
+      Session::addMessageAfterRedirect('Unauthorized file type', false, ERROR);
+      die(403);
+   }
+
+   $data = file_get_contents($_POST['img']);
+   // Save image to tmp
+   $file = fopen(GLPI_TMP_DIR . '/' . $file_name, 'wb');
+   fwrite($file, $data);
+   fclose($file);
+
+   // Add document and link. Adding the document will cleanup the temp file for us.
+   $doc = new Document();
+   $doc_id = $doc->add([
+      'name'         => $file_name,
+      '_filename'    => [$file_name]
+   ]);
+   $doc_item = new Document_Item();
+   $doc_item->add([
+      'documents_id' => $doc_id,
+      'itemtype'     => $_POST['itemtype'],
+      'items_id'     => $_POST['items_id'],
+   ]);
 
 // In case something fails and the temp file remains, remove it
-if (file_exists(GLPI_TMP_DIR . '/' . $file_name)) {
-   unlink(GLPI_TMP_DIR . '/' . $file_name);
+   if (file_exists(GLPI_TMP_DIR . '/' . $file_name)) {
+      unlink(GLPI_TMP_DIR . '/' . $file_name);
+   }
+} else if (isset($_FILES['blob'])) {
+   // Handle screen recording upload
+
+   // Name format: Screen Recording + Timestamp + random 5 character hex + extension
+   $file_name = 'Screen Recording ' . $_SESSION['glpi_currenttime'] . '-' . sprintf('%05X', random_int(0, 1048575)) . '.' . $ext;
+   if (!Document::isValidDoc($file_name)) {
+      Session::addMessageAfterRedirect('Unauthorized file type', false, ERROR);
+      die(403);
+   }
+
+   $data = $_FILES['blob'];
+
+   if ($data['error'] === UPLOAD_ERR_OK) {
+      move_uploaded_file($data['tmp_name'], GLPI_TMP_DIR . '/' . $file_name);
+
+      // Add document and link. Adding the document will cleanup the temp file for us.
+      $doc = new Document();
+      $doc_id = $doc->add([
+         'name'         => $file_name,
+         '_filename'    => [$file_name]
+      ]);
+      $doc_item = new Document_Item();
+      $doc_item->add([
+         'documents_id' => $doc_id,
+         'itemtype'     => $_POST['itemtype'],
+         'items_id'     => $_POST['items_id'],
+      ]);
+
+      // In case something fails and the temp file remains, remove it
+      if (file_exists($data['tmp_name'])) {
+         unlink($data['tmp_name']);
+      }
+      if (file_exists(GLPI_TMP_DIR . '/' . $file_name)) {
+         unlink(GLPI_TMP_DIR . '/' . $file_name);
+      }
+   }
 }
