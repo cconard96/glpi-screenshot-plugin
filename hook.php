c0f8f6a4975ddb6f6cd06609946c93c2568eb36b
cconard96 <cconard96@gmail.com>
2020-10-16 21:52:01 -0400

    Move HTTPS check
    With it in the prerequisite check, it gets evaluated every page load and if anyone is not using HTTPS, it disables the plugin.
    This should only be checked at install in the hopes of warning the Administrator that HTTPS is required.

--- hook.php
+++ hook.php
@@ -22,6 +22,10 @@
 
 function plugin_screenshot_install()
 {
+   if (empty($_SERVER['HTTPS']) && !in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+      echo 'This plugin requires GLPI be served over HTTPS';
+      return false;
+   }
 	return true;
 }
 
--- setup.php
+++ setup.php
@@ -66,10 +66,6 @@ function plugin_screenshot_check_prerequisites()
          return false;
       }
    }
-   if (empty($_SERVER['HTTPS']) && !in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
-      echo 'This plugin requires GLPI be served over HTTPS';
-      return false;
-   }
    return true;
 }
 
